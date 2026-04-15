57f8e40e1988391768d0be1ed45d1a9c168fb799
NickZmh <nickzmh92@gmail.com>
2026-04-15 17:44:26 +0200

    add description for properties

--- src/modules/images/dto/get-upload-url.dto.ts
+++ src/modules/images/dto/get-upload-url.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class GetUploadUrlDto {
+  @ApiProperty({
+    example: 'image/png',
+    description: 'The MIME type of the file that will be uploaded.',
+  })
   @IsString()
   fileType!: string;
 }
--- src/modules/images/images.controllers.ts
+++ src/modules/images/images.controllers.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { GetUploadUrlDto } from './dto/get-upload-url.dto';
 import { ImagesService, UploadUrlResponse } from './images.service';
+import { ApiBody, ApiOkResponse } from '@nestjs/swagger';
+import { UploadUrlResponseDto } from './dto/image.dto';
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
   @Post('upload-url')
+  @ApiOkResponse({ type: UploadUrlResponseDto })
+  @ApiBody({ type: GetUploadUrlDto })
   async getUploadUrl(@Body() dto: GetUploadUrlDto): Promise<UploadUrlResponse> {
     return this.imagesService.getUploadUrl(dto.fileType);
   }
